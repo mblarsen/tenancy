78738a813de5e3fc6ee4a08cd5c0550278f9ab8b
Daniël Klabbers <daniel@klabbers.email>
2019-09-03 22:19:30 +0200

    take into consideration deletion events

--- src/Hooks/Webserver/Hooks/WebserverMutation.php
+++ src/Hooks/Webserver/Hooks/WebserverMutation.php
@@ -18,6 +18,7 @@ namespace Tenancy\Hooks\Webserver\Hooks;
 
 use Illuminate\Contracts\View\Factory;
 use Tenancy\Lifecycle\Hook;
+use Tenancy\Tenant\Events\Deleted;
 
 class WebserverMutation extends Hook
 {
@@ -25,11 +26,27 @@ class WebserverMutation extends Hook
     {
         event($event = new Configuring($this->event->tenant));
 
+        if ($this->event instanceof Deleted) {
+            $this->delete($event);
+        } else {
+            $this->write($event);
+        }
+    }
+
+    protected function write(Configuring $event)
+    {
         /** @var Factory $factory */
         $factory = resolve(Factory::class);
 
         $view = $factory->file($event->view, array_merge(['tenant' => $this->event->tenant], $event->data));
 
         file_put_contents($event->path, $view->render());
+    }
+
+    protected function delete(Configuring $event)
+    {
+        if (file_exists($event->path)) {
+            unlink($event->path);
+        }
     }
 }
